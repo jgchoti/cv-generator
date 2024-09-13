405110772f9af42094d3107b8060d280e11d5d43
jgchoti <jgchotirat@gmail.com>
2024-09-13 15:33:30 +0200

    Create icons.js

---
+++ src/utils/icons.js
@@ -0,0 +1,29 @@
+import {
+    faUser,
+    faEnvelope,
+    faPhone,
+    faAddressBook, faSchool, faGraduationCap, faMagnifyingGlass, faCalendarDays, faDesktop,
+    faPenToSquare
+} from "@fortawesome/free-solid-svg-icons";
+import {
+    faFacebook, faGithub, faLinkedin, faInstagram
+} from "@fortawesome/free-brands-svg-icons"
+
+
+export const icons = {
+    name: faUser,
+    email: faEnvelope,
+    phone: faPhone,
+    address: faAddressBook,
+    school: faSchool,
+    degree: faGraduationCap,
+    fieldOfStudy: faMagnifyingGlass,
+    startDate: faCalendarDays,
+    endDate: faCalendarDays,
+    facebook: faFacebook,
+    github: faGithub,
+    linkedin: faLinkedin,
+    instagram: faInstagram,
+    website: faDesktop,
+    description: faPenToSquare
+};
