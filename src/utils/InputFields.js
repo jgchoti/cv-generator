fb24c9db3029cf9d609c084ab824107994b5fafb
jgchoti <jgchotirat@gmail.com>
2024-09-13 15:33:22 +0200

    Create InputFields.js

---
+++ src/utils/InputFields.js
@@ -0,0 +1,190 @@
+export const educationFields = [
+    {
+        name: "school",
+        type: "text",
+        value: "school",
+        placeholder: "School",
+        required: true,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "degree",
+        type: "text",
+        value: "",
+        placeholder: "Degree",
+        required: true,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "fieldOfStudy",
+        type: "text",
+        value: "",
+        placeholder: "Field of study",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "current",
+        type: "checkbox",
+        placeholder: "Current Study",
+        value: false,
+        required: false,
+        isTextarea: false,
+        isCheckbox: true
+    },
+    {
+        name: "startDate",
+        type: "date",
+        value: "",
+        placeholder: "Start date...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "endDate",
+        type: "date",
+        value: "",
+        placeholder: "End date (or expected)...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "description",
+        type: "text",
+        value: "",
+        placeholder: "Description",
+        required: false,
+    },
+];
+
+export const experienceFields = [
+    {
+        name: "school",
+        type: "text",
+        placeholder: "School",
+        required: true,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "degree",
+        type: "text",
+        placeholder: "Degree",
+        required: true,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "fieldOfStudy",
+        type: "text",
+        placeholder: "Field of study",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "currently",
+        type: "checkbox",
+        placeholder: "Current Job",
+        required: false,
+        isTextarea: false,
+        isCheckbox: true
+    },
+    {
+        name: "startDate",
+        type: "date",
+        placeholder: "Start date...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "endDate",
+        type: "date",
+        placeholder: "End date (or expected)...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "description",
+        type: "text",
+        placeholder: "Description",
+        required: false,
+        isTextarea: true,
+        isCheckbox: false
+    },
+]
+
+export const InformationFields = [
+    {
+        name: "name",
+        type: "text",
+        placeholder: "Enter your name",
+        required: true,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "description",
+        type: "text",
+        placeholder: "Enter your headline...",
+        required: false,
+        isTextarea: true,
+        isCheckbox: false
+    },
+    {
+        name: "email",
+        type: "email",
+        placeholder: "Enter your email...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "phone",
+        type: "text",
+        placeholder: "Enter your phone...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "address",
+        type: "text",
+        placeholder: "Enter your address...",
+        required: false,
+        isTextarea: true,
+        isCheckbox: false
+    },
+    {
+        name: "linkedin",
+        type: "text",
+        placeholder: "Enter your linkedin...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "github",
+        type: "text",
+        placeholder: "Enter your github...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+    {
+        name: "website",
+        type: "text",
+        placeholder: "Enter your website...",
+        required: false,
+        isTextarea: false,
+        isCheckbox: false
+    },
+
+];
